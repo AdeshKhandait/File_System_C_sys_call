dc40cc3a4937c3162bd0a99d02fd4363f84dcf9a
adesh <inbx.adesh@gmail.com>
2022-10-03 18:09:25 +0530

    feat: Bit Map Implemented for MetaData and Disk Block

--- src/create_file_sytem.c
+++ src/create_file_sytem.c
@@ -1,75 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<fcntl.h> 
-#include<errno.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "file_system.h"
-
-// Basic Information
-// 1GB = 1000000000
-// Empty = -1
-// Occupied = -2
-
-
-// ---------------------------------------------------------------Variables----------------------------------------------------------
-
-// Define the MetaData Variables
-
-// Size of the HDD
-long int sizeHDD;
-
-// Size of Start Block
-long int size_Start_Block;
-
-// HDD - Size of Start_Block
-long int sizeHDD_StartBlock;
-
-// Size of the MetaData
-long int size_MetaData;
-
-// Size of the DiskBlock
-long int size_Disk_Block;
-
-// MetaData + DiskBlock
-long int size_MetaData_DiskBlock;
-
-// Number of MetaData
-long int num_MetaData;
-
-// Number of the DiskBlock
-long int num_DiskBlock;
-
-// Availabel Space in Disk
-long int Space;
-
-
-//---------------------------------------------------------------Declaring the Structs----------------------------------------------
-struct StartBlock SB;
-struct MetaData *Inodes;
-struct Disk_Block *DBK;
-
-
 
 //-------------------------------------------------------------------Functions-------------------------------------------------------
 
 // Creating the file System
-void create_file_system() {
+void create_file_system()
+{
 
-//-------------------------------------------------------------------File System Size-------------------------------------------------------
+    //-------------------------------------------------------------------File System Size-------------------------------------------------------
     printf("Enter the File Size in GB: ");
     sizeHDD = input_long_int();
 
-    //Converting into bytes
-    sizeHDD = sizeHDD*1000000000;
-    printf("sizeHDD: %ld\n",sizeHDD);
+    // Converting into bytes
+    sizeHDD = sizeHDD * 1000000000;
+    printf("sizeHDD: %ld\n", sizeHDD);
 
     // Size of the StartBlock
     size_Start_Block = sizeof(struct StartBlock);
-    printf("Size of the StartBlock: %ld\n",size_Start_Block);
+    printf("Size of the StartBlock: %ld\n", size_Start_Block);
 
     // sizeHDD - StartBlock
     sizeHDD_StartBlock = sizeHDD - size_Start_Block;
-    printf("sizeHDD - StartBlock: %ld\n",sizeHDD_StartBlock);
+    printf("sizeHDD - StartBlock: %ld\n", sizeHDD_StartBlock);
 
     // Size of the MetaData
     size_MetaData = sizeof(struct MetaData);
@@ -91,22 +47,20 @@ void create_file_system() {
     num_DiskBlock = num_MetaData;
     printf("Number of the Disk_Block: %ld\n", num_DiskBlock);
 
+    //-------------------------------------------------------------------SettingUPMetaData-------------------------------------------------------
 
-//-------------------------------------------------------------------SettingUPMetaData-------------------------------------------------------
-
-//Inserting the Data into Start Block
+    // Inserting the Data into Start Block
     SB.size_HDD = sizeHDD;
-    printf("SB.size_HDD: %ld\n",SB.size_HDD);
+    printf("SB.size_HDD: %ld\n", SB.size_HDD);
     SB.start_MetaData = size_Start_Block;
-    printf("SB.start_MetaData: %ld\n",SB.start_MetaData);
-    SB.start_Data_Block = num_MetaData*size_MetaData;
-    printf("SB.start_Data_Block: %ld\n",SB.start_Data_Block);
+    printf("SB.start_MetaData: %ld\n", SB.start_MetaData);
+    SB.start_Data_Block = num_MetaData * size_MetaData;
+    printf("SB.start_Data_Block: %ld\n", SB.start_Data_Block);
     SB.size_block = BLOCKSIZE;
-    printf("SB.size_block: %d\n",SB.size_block);
+    printf("SB.size_block: %d\n", SB.size_block);
 
-
-// Creating the Array in of MetaData
-Inodes = malloc(sizeof(struct MetaData)*num_MetaData);
+    // Creating the Array in of MetaData
+    Inodes = malloc(sizeof(struct MetaData) * num_MetaData);
 
     // Inserting the data into MetaData
     for (int i = 0; i < num_MetaData; i++)
@@ -114,11 +68,11 @@ Inodes = malloc(sizeof(struct MetaData)*num_MetaData);
         Inodes[i].file_number = -1;
         Inodes[i].file_size = -1;
         Inodes[i].first_Block = -1;
-        strcpy(Inodes[i].file_name,"Empty");
+        strcpy(Inodes[i].file_name, "Empty");
     }
-    
-// Creating the Array of the MetaData
-DBK = malloc(sizeof(struct Disk_Block)*num_DiskBlock);
+
+    // Creating the Array of the MetaData
+    DBK = malloc(sizeof(struct Disk_Block) * num_DiskBlock);
 
     // Inserting the Data into DiskBlock
     for (int i = 0; i < num_MetaData; i++)
@@ -127,61 +81,91 @@ DBK = malloc(sizeof(struct Disk_Block)*num_DiskBlock);
         DBK[i].pos = 0;
     }
 
-
-//-------------------------------------------------------------------Creating the File System-------------------------------------------------------
+    //-------------------------------------------------------------------Creating the File System-------------------------------------------------------
 
     create_file();
     sync_file_system();
     printf("\nFile_System_Successfully_Created!!\n");
 }
 
-void sync_file_system(){
+void sync_file_system()
+{
 
-// Opening the File
+    // Opening the File
     int file = open_file();
 
-// Writing the Start Block
-int wrt_SB = write(file,&SB,size_Start_Block);
-    if ( wrt_SB == size_Start_Block)
+    // Writing the Start Block
+    int wrt_SB = write(file, &SB, size_Start_Block);
+    if (wrt_SB == size_Start_Block)
     {
         printf("\nStart Block Successfully Written!\n");
     }
-    else{
+    else
+    {
         perror("Error in Writing the Start Block!\n");
     }
 
-// Writing the MetaData
+    // Writing the MetaData
     long int wrt_IN = 0;
-    
+
     for (long int i = 0; i < num_DiskBlock; i++)
     {
-       long int temp2 =  write(file,&Inodes[i],size_MetaData);
+        long int temp2 = write(file, &Inodes[i], size_MetaData);
         wrt_IN = wrt_IN + temp2;
     }
-    
-    if (wrt_IN == size_MetaData*num_MetaData)
+
+    if (wrt_IN == size_MetaData * num_MetaData)
     {
         printf("\nMetaData Successfully Written!\n");
     }
-    else{
+    else
+    {
         perror("Error in Writing the MetaData!\n");
-    }   
-    
-// Writing the Disk_Block
+    }
+
+    // Writing the Disk_Block
     long int wrt_DSK = 0;
     for (long int i = 0; i < num_DiskBlock; i++)
     {
-       long int temp2 =  write(file,&DBK[i],size_Disk_Block);
+        long int temp2 = write(file, &DBK[i], size_Disk_Block);
         wrt_DSK = wrt_DSK + temp2;
     }
-    
-    if (wrt_DSK == size_Disk_Block*num_DiskBlock)
+
+    if (wrt_DSK == size_Disk_Block * num_DiskBlock)
     {
         printf("\nDiskBlock Successfully Written!\n");
     }
-    else{
+    else
+    {
         perror("Error in Writing the DiskBlock!\n");
     }
-
 }
 
+void format_file_system()
+{
+
+    // Super Remains Same and Intial File System Parameter will be remain same
+
+    // Resetting the MetaData
+
+    // Inserting the data into MetaData
+    for (int i = 0; i < num_MetaData; i++)
+    {
+        Inodes[i].file_number = -1;
+        Inodes[i].file_size = -1;
+        Inodes[i].first_Block = -1;
+        strcpy(Inodes[i].file_name, "Empty");
+    }
+
+    // Resetting the DiskBlock
+    // Inserting the Data into DiskBlock
+    for (int i = 0; i < num_MetaData; i++)
+    {
+        DBK[i].Next_Data_Block = -1;
+        DBK[i].pos = 0;
+    }
+
+    // Updating the DISK
+    sync_file_system();
+    printf("\nFile System Successfully Cleaned!\n");
+}
--- src/file_system.h
+++ src/file_system.h
@@ -1,8 +1,10 @@
 // Meta Information about ther File System
+// Assuming Zero Indexing
 
 #define BLOCKSIZE 1024
 #define NAME 256
 
+//-------------------------------------------------------------------Structures-------------------------------------------------------
 // StartBlock -> Stores Information
 // - Size of File System
 // - Start of MetaData
@@ -39,6 +41,48 @@ struct Disk_Block
 };
 
 
+// Basic Information
+// 1GB = 1000000000
+// Empty = -1
+// Occupied = -2
+
+
+// ---------------------------------------------------------------Variables----------------------------------------------------------
+
+// Define the MetaData Variables
+
+// Size of the HDD
+long int sizeHDD;
+
+// Size of Start Block
+long int size_Start_Block;
+
+// HDD - Size of Start_Block
+long int sizeHDD_StartBlock;
+
+// Size of the MetaData
+long int size_MetaData;
+
+// Size of the DiskBlock
+long int size_Disk_Block;
+
+// MetaData + DiskBlock
+long int size_MetaData_DiskBlock;
+
+// Number of MetaData
+long int num_MetaData;
+
+// Number of the DiskBlock
+long int num_DiskBlock;
+
+// Availabel Space in Disk
+long int Space;
+
+//---------------------------------------------------------------Declaring the Structs----------------------------------------------
+struct StartBlock SB;
+struct MetaData *Inodes;
+struct Disk_Block *DBK;
+
 //----------------------------------------Declaration of the File System Operation---------------------------------------------------
 
 // Creating the File System
@@ -47,8 +91,10 @@ void create_file_system();
 // Sync the File System
 void sync_file_system();
 
+// Format the File system
+void format_file_system();
 
-// --------------------------------------------------Input_Output_Handler-----------------------------------------------------------------
+// --------------------------------------------------IO_Open_Close-----------------------------------------------------------------
 
 long int input_long_int();
 
@@ -57,3 +103,46 @@ void create_file();
 int open_file();
 
 
+//-------------------------------------------------------------------BitMap-------------------------------------------------------
+
+
+// DISK BLOCK
+long int map_size_DBK;
+long int *BitMap_DBK;
+
+// Set all the Bit
+void set_all_bit_DBK();
+
+// Create the Bit map
+void create_empty_bit_map_DBK();
+
+// Bit 64 Bit Block
+void print_bit_DBK(long int number);
+
+// Search Empty Bit
+long int search_empty_DBK();
+
+// Set the Bit Disk BLock
+void Clear_Bit_DBK(long int number);
+
+
+
+// INODES
+// DISK BLOCK
+long int map_size_IN;
+long int *BitMap_IN;
+
+// Set all the Bit
+void set_all_bit_IN();
+
+// Create the Bit map
+void create_empty_bit_map_IN();
+
+// Bit 64 Bit Block
+void print_bit_IN(long int number);
+
+// Search Empty Bit
+long int search_empty_IN();
+
+// Set the Bit Disk BLock
+void Clear_Bit_IN(long int number);
